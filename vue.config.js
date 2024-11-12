a2af8431ddb6b90999b28f460b35d2f87fca4a75
soraincloud <soraincloud@gmail.com>
2024-11-13 01:52:09 +0800

    部署分支

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/',
-  devServer:
-  {
-    port: 35555
-  },
-})
