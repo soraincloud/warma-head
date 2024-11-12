94d552c101cf8b36d4eeb91249bf9ab919ab5468
soraincloud <soraincloud@gmail.com>
2024-11-13 02:06:16 +0800

    fix：修改了编译中会出现的部分问题

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: './',
+  pwa:
+  {
+    iconPaths:
+    {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  },
   devServer:
   {
     port: 35555
